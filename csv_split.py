f358105b4971e23301fc55d10b99dcecd9430a7e
Satoshi Tohda <moshi811@gmail.com>
2023-02-06 00:05:52 +0900

    implement sentence splitting

--- csv_split.py
+++ csv_split.py
@@ -1,4 +1,8 @@
 import csv
+import kuzukiri
+
+
+segmenter = kuzukiri.Segmenter()
 
 all_lines = []
 
@@ -12,22 +16,36 @@ with open("../keigo_data/data.csv", encoding='utf-16') as f:
 #     print(key, value)
 
 new_all_lines = []
-for line in all_lines:
+task_header_list = ['タスク 1\n[件名] 本文', 'タスク 3\n[件名] 本文', 'タスク 4\n[件名] 本文']
+
+for index_line, line in enumerate(all_lines):
     if line['母語'] == '日本語':
         # print('---------------------------------------------------')
         # print(line['タスク 1\n[件名] 本文'])
         #
         # prune to 依頼タスク (1, 3, 4)
-        new_dict = {
-            'Task 1': line['タスク 1\n[件名] 本文'],
-            'Task 3': line['タスク 3\n[件名] 本文'],
-            'Task 4': line['タスク 4\n[件名] 本文']
-        }
-        new_all_lines.append(new_dict)
+
+        for task_number, task_name in enumerate(task_header_list):
+            task_text = line[task_name]
+            print(task_text)
+
+            sentences = segmenter.split(task_text)
+            sentences = [x.strip() for x in sentences]
+            sentences = [x for x in sentences if x != '']
+
+            print(sentences)
+
+            for sentence in sentences:
+                new_dict = {
+                    'Person ID': index_line,
+                    'Task ID': task_number,
+                    'Line': sentence
+                }
+                new_all_lines.append(new_dict)
 
 
 with open("../keigo_data/data_for_annotation.csv", 'w') as f:
-    fieldnames = ['Task 1', 'Task 1 Inf', 'Task 3', 'Task 3 Inf', 'Task 4', 'Task 4 Inf']
+    fieldnames = ['Person ID', 'Task ID', 'Line', 'Inf']
     writer = csv.DictWriter(f, fieldnames=fieldnames, restval='')
     writer.writeheader()
     for line in new_all_lines:
