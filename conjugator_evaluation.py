6b8eea9dd9fd7e898119e8d603cd31e02a08ece9
Satoshi Tohda <moshi811@gmail.com>
2023-02-06 13:31:08 +0900

    implement gpt result tokenization for evaluation

--- conjugator_evaluation.py
+++ conjugator_evaluation.py
@@ -5,7 +5,7 @@ import evaluate
 
 
 def eval_conjugator():
-    conjugated_pkl_path = 'data/conjugated_pred.pkl'
+    conjugated_pkl_path = 'data/pred_all.pkl'
 
     # load, organize, and tokenize data
     with open(conjugated_pkl_path, 'rb') as f:
@@ -23,10 +23,29 @@ def eval_conjugator():
 
     sacrebleu = evaluate.load("sacrebleu")
 
-    for task_no in (0, 1, 2):
-        for conj_type in ('Conjugator_polite_pred', 'Conjugator_formal_pred', 'Inf_kyt'):
-            gold_sent, pred_sent = dump_all(all_emails, 'Form_kyt', conj_type, task_no)
 
+    for email_dict in all_emails:
+        if 'GPT_pred_kyt' not in email_dict:
+            email_dict['GPT_pred_kyt'] = []
+            email_dict['GPT_pred_kyt_ignore_short_long'] = []
+
+            for line_id, line in enumerate(email_dict['GPT_pred']):
+                gpt_kyt_tokens = mk.getWS(line)
+                gpt_kyt_line = ' '.join(gpt_kyt_tokens)
+                email_dict['GPT_pred_kyt'].append(gpt_kyt_line)
+
+                # replace with inf_kyt if length is less than 50% or longer than 200% (and above 10 characters)
+                inf_tokens = email_dict['Inf_kyt'][line_id].split()
+                if len(inf_tokens) > len(gpt_kyt_tokens)*2 or (len(inf_tokens)*2 < len(gpt_kyt_tokens) and len(gpt_kyt_tokens) > 10):
+                    email_dict['GPT_pred_kyt_ignore_short_long'].append(email_dict['Inf_kyt'][line_id])
+                else:
+                    email_dict['GPT_pred_kyt_ignore_short_long'].append(gpt_kyt_line)
+
+
+    for task_no in (0, 1, 2):
+        for conj_type in ('Conjugator_polite_pred', 'Conjugator_formal_pred', 'Inf_kyt', 'GPT_pred_kyt', 'GPT_pred_kyt_ignore_short_long'):
+
+            gold_sent, pred_sent = dump_all(all_emails, 'Form_kyt', conj_type, task_no)
             results = sacrebleu.compute(predictions=pred_sent, references=gold_sent, tokenize='none')
             print(task_no, conj_type, results)
 
